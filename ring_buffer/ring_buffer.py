36ca88e7360da03a33ecff4a5061806f8301c575
Angelo Deleon <rudy209@comcast.net>
2019-08-23 08:55:13 -0700

    ring buffer done

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,9 @@
+# Easy way?
+# import numpy as np
+# from numpy_ringbuffer import RingBuffer
+
+
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
@@ -5,7 +11,17 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # add item to storage lists
+    self.storage[self.current] = item
+    if self.current >= len(self.storage)-1:
+          self.current = 0
+    else:
+      self.current += 1
 
   def get(self):
-    pass
+    return [val for val in self.storage if val is not None]
+
+
+
+buffer = RingBuffer(5)
+buffer.get()
